000c2f3d760be319e4e32397cfa490f43c9d5cbd
Aman Kumar <imtheaman@proton.me>
2023-12-06 18:44:32 +0530

    added zsh configs and fixed bash script

--- customize.sh
+++ customize.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
-cp .config/* ~/.config/
-cp .i3/* ~/.i3/
-cp etc/* /etc/
-cp .* ~
+cp -r .config/* ~/.config/
+cp -r .i3/* ~/.i3/
+cp -r etc/* /etc/
+cp -r share/zsh/* /usr/share/zsh/
+
+# TODO: use filter command to only cp files not folders
+cp .bash* ~
+cp .gitconfig ~
+cp .X* ~
+cp .idea* ~
+cp .zsh* ~
+cp .profile ~
